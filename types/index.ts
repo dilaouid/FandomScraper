e26d181042e88a38c255f10f0facb2a0b229a920
dilaouid <dilaouid@student.42.fr>
2023-05-30 19:48:36 +0200

    change dynamic import folder

--- types/index.ts
+++ types/index.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 // the different wikis available to scrape are the different folders in the schemas folder
-const schemaDirectory = path.join(__dirname, '../schemas');
+const schemaDirectory = path.join(__dirname, '../wikia');
 
 // get the list of available wikis by getting the list of folders in the schemas folder
 export const availableWikis = fs.readdirSync(schemaDirectory, { withFileTypes: true })
