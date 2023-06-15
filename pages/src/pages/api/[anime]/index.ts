dd81712af5d469bfec0d6f27eff78455e7cb6a00
dilaouid <dilaouid@student.42.fr>
2023-06-15 23:44:21 +0200

    from next to astro

---
+++ pages/src/pages/api/[anime]/index.ts
@@ -0,0 +1,20 @@
+import { FandomScraper } from "fandomscraper";
+
+export async function get({ params }: { params: { anime: string } }) {
+    try {
+        const anime = params.anime;
+        const mie = new FandomScraper(anime, {lang: "en"}) ?? null;
+        const metadatas = await mie.getMetadata();
+      
+        return new Response(JSON.stringify(metadatas), {
+          status: 200
+        });
+    } catch (error) {
+        console.log(error);
+        return new Response(null, {
+          status: 404,
+          statusText: 'Not found'
+        });
+    }
+
+};
